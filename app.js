f09e96a8b29ac4fca71f5ecf36b0672b9124cc2f
MikeNotu <mikebateman98@gmail.com>
2022-05-12 15:20:38 -0500

    Route part 1

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-var app = angular.module("app", []);
+var app = angular.module("app", ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "cajero.html",
+            controller: "controlador",
+        })
+        .when("/productos", {
+            templateUrl: "productos.html",
+            controller: "controlador",
+        });
+});
 
 app.controller("controlador", function ($scope) {
     var lista = this;
     var n = 2;
+    var nCarrito = 1;
     lista.productos = [
         { id: 1, nombre: "Agua", precio: 20.0 },
         { id: 2, nombre: "Cafe", precio: 10.0 },
@@ -18,16 +31,35 @@ app.controller("controlador", function ($scope) {
             n++;
             lista.productos.push({ id: n, nombre: nombre, precio: precio });
 
-            lista.nombre = '';
-            lista.precio = '';
+            lista.nombre = "";
+            lista.precio = "";
         }
     };
 
     $scope.apellido = "Rivas";
 
-    lista.addAlCaarrito = function(){
-        var n = 1;
+    lista.addAlCarrito = function () {
         var id = lista.productoSeleccionado;
-    }
+        var cantidad = lista.cantidad;
+        var producto = lista.productos.find(function (obj) {
+            return obj.id == id;
+        });
 
+        if (producto != undefined && cantidad > 0) {
+            lista.carrito.push({
+                id: nCarrito,
+                nombre: producto.nombre,
+                precio: producto.precio,
+                cantidad: cantidad,
+                total: producto.precio * cantidad,
+            });
+            nCarrito++;
+        }
+    };
+    lista.getTotalCarrito = function () {
+        var total = 0;
+        lista.carrito.forEach((x) => {
+            total += x.total;
+        });
+    };
 });
