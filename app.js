fcaf56b2c2a3074ba8e627c29d381a8a254c6711
MikeNotu <mikebateman98@gmail.com>
2022-05-12 11:32:34 -0500

    Displaying properties of object list

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 var app = angular.module("app", []);
 
 app.controller("controlador", function ($scope) {
+    var lista = this;
+    lista.productos = [
+        { id: 1, nombre: "Agua", precio: 20.0 },
+        { id: 2, nombre: "Cafe", precio: 10.0 },
+    ];
+
+    lista.carrito = [];
+
+    lista.addProducto = function () {};
+
     $scope.apellido = "Rivas";
 });
